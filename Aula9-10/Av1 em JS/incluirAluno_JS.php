dcf51f989ee4bfca14d07b7c2b6aa3a1e0e4a507
Mariana Plauska <103209930+MarianaPlauska@users.noreply.github.com>
2022-11-16 21:12:18 -0300

    Aulas 9-10 - AV1 com JavaScript
    Refazendo a AV1 com uso de JavaScript.

---
+++ Aula9-10/Av1 em JS/incluirAluno_JS.php
@@ -0,0 +1,32 @@
+<?php
+
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "dawacademicomanha";
+    $mensagem = "";
+
+    if($_SERVER["REQUEST_METHOD"]=="GET")
+    {    
+        $nome = $_GET["nome"];
+        $mat= $_GET["mat"];
+        $email= $_GET["email"];
+        $cpf= $_GET["cpf"];
+
+        $conn = new mysqli ($servidor, $usario, $senha, $banco);
+
+        $sql="INSERT INTO `alunos`(`nome`, `mat`, `email`) VALUES ('$nome', '$mat', '$email')";
+
+        $result=$conn->query($sql);
+
+        echo $result;
+        echo $sql;
+
+        if ($result=true){
+            $mensagem="Deu bom mano!👍";
+        } else {
+            $mensagem="VIx! Deu ruim!😭😭";
+        }
+    }
+echo $mensagem;
+?>
